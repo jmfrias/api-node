4996e6be2663828c441afdfdcda81501d7b97913
Juan Manuel Frias <jmfrias90@gmail.com>
2021-04-10 00:39:18 -0300

    Cambio atributos a privados

--- src/models/logger.js
+++ src/models/logger.js
@@ -1,29 +1,31 @@
 'use strict'
+
 class Logger {
-    logger = {
+    static #info = require('../static/info.json')
+
+    #logger = {
         "fecha" : new Date(new Date()-3600*1000*3).toISOString(),
         "nombre" : "api-node",
-        "meta" : require('../static/info.json'),
+        "meta" : Logger.#info,
         "operacion": "",
         "resultado": "",
         "codigo" : "",
-        "mensaje" : "",        
+        "mensaje" : "",
         "datos" : ""
     }
         
     constructor(operacion, resultado, codigo, mensaje, datos) {
-        this.logger.operacion = operacion
-        this.logger.resultado = resultado
-        this.logger.codigo = codigo
-        this.logger.mensaje = mensaje
-        this.logger.datos = datos
+        this.#logger.operacion = operacion
+        this.#logger.resultado = resultado
+        this.#logger.codigo = codigo
+        this.#logger.mensaje = mensaje
+        this.#logger.datos = datos
     }
 
     getLogger() {
-        console.log(JSON.stringify(this.logger))
-        return this.logger
+        console.log(JSON.stringify(this.#logger))
+        return this.#logger
     }
-
 }
 
 module.exports = Logger
