692c2a4c73cac1c2705c2f5bd1038b27c10ff2ad
Juan Manuel Frias <jmfrias90@gmail.com>
2021-10-11 12:33:12 -0300

    Unit test for version 1.0

--- src/test/health.js
+++ src/test/health.js
@@ -12,18 +12,57 @@ const info = require('../static/info.json')
 chai.should()
 chai.use(chaiHttp)
 
-describe('TEST HealthCheck', () => {
+describe('Status HealthCheck', () => {
     describe("GET /health", () => {
         it("Returns service status", (done) => {
             chai.request(server)
                 .get("/health")
                 .end((err, response) => {
                     response.should.have.status(codes.OK)
-                    response.body.should.be.a('object')                    
+                    done()
+                })
+        })
+    })
+})
+
+describe('Body HealthCheck', () => {
+    describe("GET /health", () => {
+        it("Returns service status", (done) => {
+            chai.request(server)
+                .get("/health")
+                .end((err, response) => {
+                    response.body.should.be.a('object')
+                    done()
+                })
+        })
+    })
+})
+
+describe('Test HealthCheck', () => {
+    describe("GET /health", () => {
+        it("Returns service status", (done) => {
+            chai.request(server)
+                .get("/health")
+                .end((err, response) => {
                     response.body.should.have.property('operation').eq(operations.healthcheck)
                     response.body.should.have.property('result').eq(results.ok)
                     response.body.should.have.property('code').eq(codes.OK)
-                    response.body.should.have.property('message').eq(messages.healthcheck.ok)
+                    response.body.should.have.property('message').eq(messages.healthcheck.ok)                    
+                    done()
+                })
+        })
+    })
+})
+
+describe('Version HealthCheck', () => {
+    describe("GET /health", () => {
+        it("Returns service status", (done) => {
+            chai.request(server)
+                .get("/health")
+                .end((err, response) => {
+                    response.body.should.have.property('meta')
+                        .and.a('object')
+                        .and.have.property('version').eq('v1.0')
                     done()
                 })
         })
